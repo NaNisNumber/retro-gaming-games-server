ca5f3cb3c6decd125ea16d94ecafc225e7781f8c
Tobirama <grigoroscuta.sergiu@yahoo.com>
2023-07-13 02:46:41 +0300

    add fragmentApiGamesData() that will separate the games in multiple arrays of 20/20 it it's possible and each array will represent a page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const request = require("request");
 const http = require("http");
+const url = require("url");
 let apiData;
-
+let gamesData = [];
+let lastPage;
 const gameOptionsReq = {
   url: "https://api.igdb.com/v4/games",
   method: "POST",
@@ -16,15 +18,49 @@ const gameOptionsReq = {
 request(gameOptionsReq, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     const parsedApiData = JSON.parse(body);
-    apiData = parsedApiData;
+
+    function fragmentApiGamesData() {
+      apiData = parsedApiData;
+      let fragmentedGamesData = [];
+
+      for (let i = 0; i < apiData.length; i++) {
+        const gameData = apiData[i];
+        const lastIteration = apiData.length - 1;
+        fragmentedGamesData.push(gameData);
+        // check when fragmentedGamesData have a maximum of 20 games and push the array to the gamesData;
+        if (fragmentedGamesData.length === 20) {
+          gamesData.push(fragmentedGamesData);
+          fragmentedGamesData = [];
+        }
+        // if there are are not 20 games in the fragmentedGamesData and you are at the last iteration of the loop
+        // push the fragmentedGamesData array with the remaining games to the gamesData;
+        if (i === lastIteration) {
+          gamesData.push(fragmentedGamesData);
+          fragmentedGamesData = [];
+        }
+      }
+
+      lastPage = gamesData.length - 1;
+    }
+    fragmentApiGamesData();
   }
 });
 
 const serverForSendingAllGames = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.end(JSON.stringify(apiData));
+  const { pathname, query } = url.parse(req.url, true);
+  if (pathname === "/gamesData") {
+    const queryValue = +query.game;
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.end(JSON.stringify(gamesData[`${queryValue}`]));
+  }
+  if (pathname === "/id-for-last-page") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.end(JSON.stringify(lastPage));
+  }
 });
 
 serverForSendingAllGames.listen(process.env.PORT || 5000, () => {});
